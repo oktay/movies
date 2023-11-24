c416f3949b153453153551dacfa59d76dffa9727
oktay <oktaycolakoglu@gmail.com>
2023-11-24 15:39:07 +0300

    Add toppbar loader

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Spinner from "@/components/spinner";
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-screen text-4xl">
-      <Spinner />
-    </div>
-  );
-}
