8b585c1adc10cabc5aab1567abf101f32f2792c4
oktay <oktaycolakoglu@gmail.com>
2024-06-30 09:39:45 +0300

    Remove unnecessary caches

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,7 @@ import { cache } from "react";
 export const getListItem = (type: MediaType, query: Query) =>
   LISTS[type].find((item) => item.query === query);
 
-export const fetchApi = cache(async (path: string, params?: any) => {
+export const fetchApi = async (path: string, params?: any, options?: RequestInit) => {
   const url = new URL(API_URL);
   const searchParams = new URLSearchParams({
     ...params,
@@ -15,40 +15,34 @@ export const fetchApi = cache(async (path: string, params?: any) => {
   url.pathname += path;
   url.search = searchParams.toString();
 
-  const resp = await fetch(url.toString());
+  const resp = await fetch(url.toString(), options);
   const data = await resp?.json();
 
   if (!resp.ok || !data) throw new Error("Network Error");
 
   return data;
-});
+};
 
-export const getMedia = cache(
-  (type: MediaType, id: string): Promise<Media> =>
-    fetchApi(`/${type}/${id}`, {
-      append_to_response: "credits,images,videos,recommendations,episodes",
-      include_image_language: "en",
-    })
-);
+export const getMedia = (type: MediaType, id: string): Promise<Media> =>
+  fetchApi(`/${type}/${id}`, {
+    append_to_response: "credits,images,videos,recommendations,episodes",
+    include_image_language: "en",
+  });
 
-export const getRandomMedia = cache((items: Media[]): Promise<Media> => {
+export const getRandomMedia = (items: Media[]): Promise<Media> => {
   const randomItem = items[Math.floor(Math.random() * items.length)];
   const randomItemType = randomItem.name ? "tv" : "movie";
 
   return getMedia(randomItemType, randomItem.id);
-});
+};
 
-export const getMediaEpisodes = cache(
-  (id: string, season: number): Promise<Season> =>
-    fetchApi(`/tv/${id}/season/${season}`)
-);
+export const getMediaEpisodes = (id: string, season: number): Promise<Season> =>
+  fetchApi(`/tv/${id}/season/${season}`);
 
-export const getPerson = cache(
-  (id: string): Promise<Person> =>
-    fetchApi(`/person/${id}`, {
-      append_to_response: "combined_credits,images",
-    })
-);
+export const getPerson = (id: string): Promise<Person> =>
+  fetchApi(`/person/${id}`, {
+    append_to_response: "combined_credits,images",
+  });
 
 export const getSearch = (
   query: string,
@@ -63,6 +57,10 @@ export const getTrending = cache(
   (type: MediaType, page: number | string = 1): Promise<PageResult<Media>> =>
     fetchApi(`/trending/${type}/week`, {
       page,
+    }, {
+      next: {
+        revalidate: 60 * 60 * 24,
+      }
     })
 );
 
@@ -70,34 +68,39 @@ export const getQuery = cache(
   (query: QueryItem, page: number | string = 1): Promise<PageResult<Media>> =>
     fetchApi(`/${query.type}/${query.query}`, {
       page,
+    }, {
+      next: {
+        revalidate: 60 * 60 * 24,
+      }
     })
 );
 
 export const getGenreList = cache(
-  (type: MediaType): Promise<GenreList> => fetchApi(`/genre/${type}/list`)
+  (type: MediaType): Promise<GenreList> => fetchApi(`/genre/${type}/list`, undefined, {
+    next: {
+      revalidate: 60 * 60 * 24,
+    }
+  })
 );
 
-export const getGenre = cache(
-  (
-    type: MediaType,
-    id: number,
-    page: number | string = 1
-  ): Promise<PageResult<Media>> =>
-    fetchApi(`/discover/${type}`, {
-      page,
-      with_genres: id,
-    })
-);
+export const getGenre = (
+  type: MediaType,
+  id: number,
+  page: number | string = 1
+): Promise<PageResult<Media>> =>
+  fetchApi(`/discover/${type}`, {
+    page,
+    with_genres: id,
+  });
 
 export const getAvailableRegions = cache(
   (): Promise<PageResult<Region>> => fetchApi(`/watch/providers/regions`)
 );
 
-export const getProviders = cache(
-  (type: MediaType, id: string): Promise<ProviderResult> =>
-    fetchApi(`/${type}/${id}/watch/providers`)
-);
+export const getProviders = (
+  type: MediaType,
+  id: string
+): Promise<ProviderResult> => fetchApi(`/${type}/${id}/watch/providers`);
 
-export const getCollection = cache(
-  (id: string): Promise<CollectionResult> => fetchApi(`/collection/${id}`)
-);
+export const getCollection = (id: string): Promise<CollectionResult> =>
+  fetchApi(`/collection/${id}`);
