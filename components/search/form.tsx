5f0818c34734bdd0b9d52c3e56dbcf95b7a6635f
oktay <oktaycolakoglu@gmail.com>
2024-07-02 10:55:54 +0300

    add ga search event

--- components/search/form.tsx
+++ components/search/form.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { sendGAEvent } from "@next/third-parties/google";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function SearchForm({ query }: { query?: string }) {
   const router = useRouter();
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setTimeout(() => router.replace(`/search?q=${e.target.value}`), 1000);
+  }
+
+  useEffect(() => {
+    if (!query) return;
+    sendGAEvent({
+      event: "search",
+      search_term: query,
+    })
+  }, [query]);
 
   return (
     <input
@@ -12,9 +26,7 @@ export default function SearchForm({ query }: { query?: string }) {
       className="w-full text-2xl p-4 bg-zinc-800 border border-transparent border-b-zinc-700 focus:outline-none focus:border focus:border-theme-500"
       placeholder="Search..."
       defaultValue={query}
-      onChange={(e) =>
-        setTimeout(() => router.replace(`/search?q=${e.target.value}`), 500)
-      }
+      onChange={handleChange}
       required
       autoFocus
     />
